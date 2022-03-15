b253009eb3d21e416de8e529cd8ff498e1fe129a
Kevin Mason <krmason2020@gmail.com>
2022-03-14 21:14:22 -0700

    able to fetch results from countries api and display the name in the country element

--- src/components/Country.js
+++ src/components/Country.js
@@ -1,8 +1,26 @@
-import React from "react";
+import { React, useEffect, useState } from "react";
 import "../styles.css";
 
 const Country = () => {
-  return <div className="Country">Country</div>;
+  const [country, setCountry] = useState("");
+  useEffect(() => {
+    fetch("https://restcountries.com/v2/name/canada")
+      //brings back an array of results,
+      .then((response) => response.json())
+      .then((countries) => {
+        console.log(countries);
+        setCountry(countries[0]);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+  return (
+    <div className="Country">
+      <h1>{country.name}</h1>
+      {/* <img src="" alt="" /> */}
+    </div>
+  );
 };
 
 export default Country;
