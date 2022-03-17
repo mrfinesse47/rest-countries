7d9764c4f56c41238716219d7d02142666aabbb5
Kevin Mason <krmason2020@gmail.com>
2022-03-16 22:06:42 -0700

    got a decent looking country card working after realizing they want you to crop the flag rather than display it in its natural aspect ratio

--- src/components/Country.js
+++ src/components/Country.js
@@ -3,36 +3,56 @@ import "../styles.css";
 
 const Country = () => {
   const [country, setCountry] = useState("");
+  const [isLoading, setLoading] = useState(true);
   useEffect(() => {
-    fetch("https://restcountries.com/v2/name/canada")
+    fetch("https://restcountries.com/v2/name/usa")
       //brings back an array of results,
       .then((response) => response.json())
       .then((countries) => {
         console.log(countries);
         setCountry(countries[0]);
+        setLoading(false);
       })
       .catch((err) => {
         console.log(err);
       });
   }, []);
   return (
-    <div className={`Country ${"Country-light"}`}>
-      <img src={country.flag} alt={country.name} />
-      <h1>{country.name}</h1>
-      <p>
-        <strong>Population:</strong>
-        {country.population}
-      </p>
-      <p>
-        <strong>Region:</strong>
-        {country.region}
-      </p>
-      <p>
-        <strong>Capital:</strong>
-        {country.capital}
-      </p>
-      {/* <img src="" alt="" /> */}
-    </div>
+    <>
+      {isLoading ? (
+        <div>loading</div>
+      ) : (
+        <div className={`Country ${"Country-light"}`}>
+          <figure>
+            <img src={country.flag} alt={country.name} />
+          </figure>
+          {/* <img
+            src="https://flagcdn.com/h240/ca.png"
+            height="240"
+            alt="Canada"
+          ></img> */}
+
+          <div className="container">
+            <h2>{country.name}</h2>
+            <div className="details">
+              <p>
+                <strong>Population: </strong>
+                {country.population}
+              </p>
+              <p>
+                <strong>Region: </strong>
+                {country.region}
+              </p>
+              <p>
+                <strong>Capital: </strong>
+                {country.capital}
+              </p>
+            </div>
+          </div>
+          {/* <img src="" alt="" /> */}
+        </div>
+      )}
+    </>
   );
 };
 
