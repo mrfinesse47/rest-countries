6c79fb56d031a2b2a5bcdb778b6a4ea6f3d4c186
Kevin Mason <krmason2020@gmail.com>
2022-03-17 21:43:23 -0700

    added fa icons to the project via npm

--- src/components/CountryList.js
+++ src/components/CountryList.js
@@ -1,32 +1,39 @@
 import Country from "./Country";
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCoffee } from "@fortawesome/free-solid-svg-icons";
 
 const CountryList = (props) => {
   //will want to filter ccountries first before mapping
   const [search, setSearch] = useState("");
+
   // const [region, setRegion]= useState("");//will worry about later
   return (
     <>
       <div className="filters">
-        <input
-          type="text"
-          value={search}
-          onChange={(event) => setSearch(event.target.value)}
-        ></input>
+        <label className="country-search">
+          <input
+            type="text"
+            value={search}
+            onChange={(event) => setSearch(event.target.value)}
+            required
+          ></input>
+          <div className="placeholder">
+            <FontAwesomeIcon icon={faCoffee} /> <div> Enter Text</div>
+          </div>
+        </label>
       </div>
       <div className="country-list">
-        {props.countries.map((country, index) => {
-          return (
-            <Country
-              key={index}
-              flag={country.flags.svg}
-              name={country.name.common}
-              population={country.population}
-              region={country.region}
-              capital={country.capital}
-            />
-          );
-        })}
+        {props.countries.map((country, index) => (
+          <Country
+            key={country.cca2}
+            flag={country.flags.svg}
+            name={country.name.common}
+            population={country.population}
+            region={country.region}
+            capital={country.capital}
+          />
+        ))}
       </div>
     </>
   );
