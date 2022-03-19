d62ca48219cdd4c6fb489f4f9334a2315dddaa1d
Kevin Mason <krmason2020@gmail.com>
2022-03-19 15:10:24 -0700

    search working for country commin name

--- src/components/CountryList.js
+++ src/components/CountryList.js
@@ -7,9 +7,7 @@ const CountryList = (props) => {
   //this will be a view in router
   //will want to filter countries first before mapping
   const [search, setSearch] = useState("");
-  const [region, setRegion] = useState("");
-
-  console.log("renders");
+  const [region, setRegion] = useState("all");
 
   return (
     <>
@@ -18,16 +16,21 @@ const CountryList = (props) => {
         <DropDown setRegion={setRegion} />
       </div>
       <main className="country-list">
-        {props.countries.map((country, index) => (
-          <Country
-            key={country.cca2}
-            flag={country.flags.svg}
-            name={country.name.common}
-            population={country.population}
-            region={country.region}
-            capital={country.capital}
-          />
-        ))}
+        {props.countries
+          .filter((country) => {
+            const regex = new RegExp(search, "gi");
+            return country.name.common.match(regex);
+          })
+          .map((country, index) => (
+            <Country
+              key={country.cca2}
+              flag={country.flags.svg}
+              name={country.name.common}
+              population={country.population}
+              region={country.region}
+              capital={country.capital}
+            />
+          ))}
       </main>
     </>
   );
