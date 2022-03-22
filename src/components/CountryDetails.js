f32ef14ed8a952ef8f40b591b2b3c9a1f061d304
Kevin Mason <krmason2020@gmail.com>
2022-03-21 20:12:29 -0700

    displaying currrencies and fixed bug for non booleam coming in from local storage

--- src/components/CountryDetails.js
+++ src/components/CountryDetails.js
@@ -2,12 +2,23 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowLeftLong } from "@fortawesome/free-solid-svg-icons";
 
-{
-  /* <i class="fa-solid fa-arrow-left-long"></i>; */
-}
+// population.toLocaleString("en-US")
 
 const CountryDetails = ({ country, setSelectedCountry }) => {
   console.log(country);
+
+  const getNameOfCurrency = (currencies) => {
+    let currenciesString = "";
+
+    for (const key in currencies) {
+      // console.log(key);
+      console.log(currencies[key].name);
+      currenciesString += " " + currencies[key].name + ",";
+    }
+    currenciesString = currenciesString.slice(0, currenciesString.length - 1);
+
+    return currenciesString;
+  };
   return (
     <div className="CountryDetails">
       <div className="button" onClick={() => setSelectedCountry(null)}>
@@ -24,9 +35,21 @@ const CountryDetails = ({ country, setSelectedCountry }) => {
         <div className="flag">
           <img src={country.flags.png} alt={country.name} />
         </div>
-        <aside className="details"></aside>
+        <aside className="details-container">
+          <div className="details-1">
+            <h1>{country.name.common}</h1>
+            <p>Native Name: {country.name.official}</p>
+            <p>Population: {country.population.toLocaleString("en-US")}</p>
+            <p>Region: {country.region}</p>
+            <p>Sub Region: {country.subregion}</p>
+          </div>
+          <div className="details-2">
+            <p>Top Level Domain: {country.tld}</p>
+            <p>Currencies: {getNameOfCurrency(country.currencies)}</p>
+          </div>
+        </aside>
+        <footer className="border-countries"></footer>
       </main>
-      CountryDetails
     </div>
   );
 };
--- src/customHooks/useStateWithLocalStorage.js
+++ src/customHooks/useStateWithLocalStorage.js
@@ -1,9 +1,12 @@
 import { useEffect, useState } from "react";
 
 const useStateWithLocalStorage = (localStorageKey, fallBack) => {
-  const [value, setValue] = useState(
-    localStorage.getItem(localStorageKey) || fallBack
-  );
+  const storageVal = localStorage.getItem(localStorageKey);
+  let isDarkMode = false;
+  if (storageVal === "true") {
+    isDarkMode = true; //brings in a string from local storage so i need to cast it to a boolean
+  }
+  const [value, setValue] = useState(isDarkMode || fallBack);
   //brings in the initial value from local storage and sets state or sets state to the fallback
 
   useEffect(() => {
