cabace2ad2d8bd85f50716a243fb5a827e864f9b
Kevin Mason <krmason2020@gmail.com>
2022-03-21 18:34:47 -0700

    created back button and styled it for country detail component

--- src/components/CountryDetails.js
+++ src/components/CountryDetails.js
@@ -1,15 +1,30 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeftLong } from "@fortawesome/free-solid-svg-icons";
+
+{
+  /* <i class="fa-solid fa-arrow-left-long"></i>; */
+}
 
 const CountryDetails = ({ country }) => {
   console.log(country);
   return (
     <div className="CountryDetails">
-      <div className="go-back"></div>
+      <div className="button">
+        <div className="container">
+          <FontAwesomeIcon
+            icon={faArrowLeftLong}
+            className="arrow-left-icon"
+            size="lg"
+          />
+          Back
+        </div>
+      </div>
       <main>
-        <div className="flag"></div>
-        <aside className="details">
+        <div className="flag">
           <img src={country.flags.png} alt={country.name} />
-        </aside>
+        </div>
+        <aside className="details"></aside>
       </main>
       CountryDetails
     </div>
